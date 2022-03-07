2d67cc544956de454d068a96f4baef59e4b989d0
Nargiz-Qaniyeva <tu7lyw21z@code.edu.az>
2022-03-07 23:40:47 +0400

    first commit

---
+++ ayinadi/ayinadi/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ayinadi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region season
+
+            string month = "avqust";
+            if (month == "dekabr" || month == "yanvar" || month == "fevral")
+            {
+                Console.WriteLine("qis ayidir");
+            }
+
+            else if (month == "mart" || month == "aprel" || month == "may")
+            {
+                Console.WriteLine("yaz ayidir");
+            }
+
+            else if (month == "iyun" || month == "iyul" || month == "avqust")
+            {
+                Console.WriteLine("yay ayidir");
+            }
+            else if (month == "sentyabr" || month == "oktyabr" || month == "noyabr")
+            {
+                Console.WriteLine("payiz ayidir");
+            }
+            else
+            {
+                Console.WriteLine("bele bir ay yoxdur");
+            }
+        }
+        #endregion
+
+    }
+}
+
